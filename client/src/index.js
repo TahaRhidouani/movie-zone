c72187749ca4138e1288c0817a578d3a90a2682d
Taha Rhidouani <37096708+TahaInc@users.noreply.github.com>
2022-06-24 15:31:47 -0400

    updated for production

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import "./index.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
